3752e8850d20ca5a94e59ecd6f074f87ad17d56c
Vladimir <bandurav@mail.ru>
2026-01-10 18:23:50 +0300

    DEVELOP1 and DEVELOP2 are servers + one client

--- time-client/Program.cs
+++ time-client/Program.cs
@@ -2,8 +2,8 @@
 using System;
 using static System.Console;
 
-const string Host = "0.pool.ntp.org";
-//const string Host = "WIN-ARAF8S50IK1";
+//const string Host = "0.pool.ntp.org";
+const string Host = "DEVELOP2";
 const int TimeOut = 5000;
 WriteLine("Time Client v1.0");            
 WriteLine("(C)2001-2019 Valer BOCAN, PhD <valer@bocan.ro>");
@@ -15,7 +15,7 @@ try
 {
     var client = new TimeEntity();
     client.Connect(Host, TimeOut);
-    Console.WriteLine(client.ToString());
+    WriteLine(client.ToString());
 }
 catch (Exception ex)
 {
--- time-server/Program.cs
+++ time-server/Program.cs
@@ -4,11 +4,28 @@ using InDepth.Infrastructure.Timing;
 using static System.Console;
 
 TimeEntity server = new();
+string Host1 = "DEVELOP1";
+string Host2 = "DEVELOP2";
 
 WriteLine("Staring Time Server...");
 server.StartService();
 WriteLine("Server started. Press any key to stop.");
-ReadKey();
+do
+{
+    ConsoleKeyInfo key = ReadKey();
+    if (key.Key == ConsoleKey.D1)
+    {
+        server.Connect(Host1, 5000);
+        WriteLine(server.ToString());
+    }
+    else if (key.Key==ConsoleKey.D2)
+    {
+        server.Connect(Host2, 5000);
+        WriteLine(server.ToString());
+    }
+    else break;
+}
+while (true);
 server.StopService();
 
 

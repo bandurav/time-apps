c0d40347c59a362e1e9994a6cb581f86abbd7565
Vladimir <bandurav@mail.ru>
2026-01-10 18:30:53 +0300

    Information message added

--- time-server/Program.cs
+++ time-server/Program.cs
@@ -10,6 +10,8 @@ string Host2 = "DEVELOP2";
 WriteLine("Staring Time Server...");
 server.StartService();
 WriteLine("Server started. Press any key to stop.");
+WriteLine($"Press 1 to connect to {Host1} server");
+WriteLine($"Press 2 to connect to {Host2} server");
 do
 {
     ConsoleKeyInfo key = ReadKey();
